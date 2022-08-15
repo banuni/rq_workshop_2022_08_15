51fe554e045166e770a89a9df9cb76eeda4a99ff
Nadav Nuni <nuni@kovrr.com>
2022-08-15 19:33:27 +0300

    initial

---
+++ MenuComponent.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export const MenuComponent = ({ cook }) => {
+  //#region formstuff...
+  const [newName, setNewName] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+  const addItem = async () => {
+    if (newName && newPrice) {
+      const x = await fetch("/api/items", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ name: newName, price: newPrice, cook }),
+      }).then((r) => r.json());
+      setNewName("");
+      setNewPrice("");
+    }
+  };
+  //#endregion
+
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch(`/api/items?`)
+      .then((r) => r.json())
+      .then((data) => setItems(data));
+  }, []);
+  return (
+    <>
+      <h2>{`Welcome to ${cook}'s Hummusia`}</h2>
+      {items.map((item) => (
+        <div key={item.name}>
+          {item.name} - {item.price}
+        </div>
+      ))}
+
+      {/*#region formstuff2*/}
+      <div>
+        <h4> Add a new menu item:</h4>
+        <input
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          placeholder="new item name"
+          />
+        <input
+          value={newPrice}
+          onChange={(e) => setNewPrice(e.target.value)}
+          placeholder="new item price"
+          />
+        <button onClick={addItem}>Add!</button>
+      </div>
+      {/*#endregion formstuff2*/}
+    </>
+  );
+};
---
+++ helpers.js
@@ -0,0 +1,13 @@
+let db = {
+  menuItems: [
+    {
+      name: "חומוס נטו",
+      price: 28,
+      cooks: ["nuni", "liraz"],
+    },
+    { name: "רימונדה", price: 9, cooks: ["nuni", "liraz"] },
+    { name: "פרגית על הפלאנצ׳ה", price: 38, cooks: ["liraz"] },
+  ],
+};
+
+export { db };
